ff4263f2ba2d4580d8fa0bdea613f8ad7c0d3d0a
Tynael Le Rhun <tle-rhun@f6r6s6.clusters.42paris.fr>
2026-02-16 12:13:39 +0100

    voir struct

--- philo.h
+++ philo.h
@@ -6,22 +6,13 @@
 /*   By: tle-rhun <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/11 10:43:53 by tle-rhun          #+#    #+#             */
-/*   Updated: 2026/02/16 11:59:36 by tle-rhun         ###   ########.fr       */
+/*   Updated: 2026/02/16 12:07:38 by tle-rhun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <pthread.h>
 #include <stdlib.h>
 #include <sys/time.h>
-
-typedef struct philosoph
-{
-	int	nb_fork;
-	int id;
-	int is_died;
-	t_info info;
-	pthread_t p;
-}		t_philo;
 
 typedef struct info
 {
@@ -33,10 +24,20 @@ typedef struct info
 	int	number_of_times_each_philosopher_must_eat;
 }		t_info;
 
+typedef struct philosoph
+{
+	int	nb_fork;
+	int id;
+	int is_died;
+	t_info info;
+	pthread_t p;
+}		t_philo;
+
+
 typedef struct global
 {
 	t_philo *philosoph; 
-}		t_glob
+}		t_glob;
 
 /* PROCESS */
 void	*routine (void* var);
